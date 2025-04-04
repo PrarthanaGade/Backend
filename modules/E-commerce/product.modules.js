65c3f65133b043150859d940eed7d92b8bff8285
GeniusPrarthana <Prarthanagade25@gmail.com>
2025-04-05 00:11:41 +0530

    finished

---
+++ modules/E-commerce/product.modules.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stocks: {
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "Category",
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }
+
+}, { timestamps: true });
+
+export const Product = mongoose.module("Product", productSchema);
